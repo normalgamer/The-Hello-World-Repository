c7ade19e6a1d8a32dad5707c31793681c268fa5e
normalgamer <36603719+normalgamer@users.noreply.github.com>
2019-11-26 18:47:34 +0100

    Update Hello World.ino

--- Arduino/Hello World.ino
+++ Arduino/Hello World.ino
@@ -4,6 +4,7 @@ void setup(){
 }
 
 void loop(){
-    digitalPin(13, 1);              // 1 = ON
+    digitalPin(13, 1);              // 1 = ON. It is also considered part of the Hello World program in Arduino to turn on the
+                                    // built-in LED on the 13th digital pin
     Serial.println("Hello World");  // Alternatively, you can put this in void setup(), so it isn't spammed on the serial monitor
 }
